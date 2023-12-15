723700710a6c383941e603aa0546ba433d94c5c2
AbdoWise-z <moh2002.abdow@gmail.com>
2023-12-15 22:08:19 +0200

    some stuff idk

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -4,14 +4,28 @@ use ieee.numeric_std.all;
 
 entity ControlUnit is
   port (
-    op_code : in std_logic_vector(4 downto 0);
-    sig     : out std_logic_vector(22 downto 0)
+    op_code  : in std_logic_vector(4 downto 0);
+    int_sig  : in std_logic_vector(3 downto 0);
+    sig      : out std_logic_vector(22 downto 0)
   ) ;
 end ControlUnit;
 
 
 architecture ControlUnitArch of ControlUnit is
     type RG_FILE_TYPE is Array(0 to 31) of std_logic_vector(22 downto 0);
+    type MG_FILE_TYPE is Array(0 to 7)  of std_logic_vector(22 downto 0);
+
+    constant INT_SIG_TABLE: MG_FILE_TYPE := (
+        "01100000000000010101000", -- RESET : Load PC , 0 (reset address)
+        "00100000000001100010100", -- INT   : Push PC
+        "00000000000001100011100", -- INT   : Push FR
+        "01100000000000010101000", -- INT   : Load PC , 2 (int address)
+        "00000000000000000000000", -- flex  : NOP
+        "00000000000110000000000", -- flex  : NOP
+        "00000000001010000000000", -- flex  : NOP
+        "00000000001110000000000"  -- flex  : NOP
+    );
+    
     constant SIGNAL_TABLE : RG_FILE_TYPE := (
         "00000000000000000000000",
         "00000000000110000000000",
@@ -47,6 +61,8 @@ architecture ControlUnitArch of ControlUnit is
         "00000000000000000000000"
     );
 begin
-    sig <= SIGNAL_TABLE(to_integer(unsigned(op_code)));
+
+    sig <= SIGNAL_TABLE(to_integer(unsigned(op_code))) when int_sig(3) = '0' 
+      else INT_SIG_TABLE(to_integer(unsigned(int_sig(2 downto 0))));
 
 end ControlUnitArch ; -- ControlUnitArch
--- PreDecoder.vhd
+++ PreDecoder.vhd
@@ -6,13 +6,19 @@ entity PreDecoder is
   port (
     clk    : in std_logic;
     flush  : in std_logic;
+    INT    : in std_logic; -- execute interrupt
+    RESET  : in std_logic; -- execute reset
     InstIn : in std_logic_vector(15 downto 0);
-    InstOut : out std_logic_vector(31 downto 0)
+    InstOut   : out std_logic_vector(31 downto 0);
+    intSigOut : out std_logic_vector(3 downto 0)
   ) ;
 end PreDecoder;
 
 architecture PreDecoderArch of PreDecoder is
-    constant nop     : std_logic_vector(31 downto 0) := (others => '0');
+    constant nop          : std_logic_vector(31 downto 0) := (others => '0');
+    constant RST_INST     : std_logic_vector(31 downto 0) := "00000000000000000000000000000000"; -- the instruction used in Load PC (I set EA to 0 by hand here)
+    constant INT_INST     : std_logic_vector(31 downto 0) := "00000000000000000000000000000010"; -- push pc
+    
     constant filler  : std_logic_vector(15 downto 0) := (others => '0');
     
     signal half : std_logic_vector(15 downto 0);
@@ -23,12 +29,22 @@ architecture PreDecoderArch of PreDecoder is
     signal MULTI_3 : std_logic_vector(4 downto 0) := "10100"; -- LDD
     signal MULTI_4 : std_logic_vector(4 downto 0) := "10101"; -- STD
 
-    -- for testing ..
-    signal latch : std_logic_vector(31 downto 0);
+    
+    signal latch_inst   : std_logic_vector(31 downto 0);
+    signal latch_intSig : std_logic_vector(3 downto 0);
+    signal delay        : std_logic_vector(2 downto 0) := "00";
+    signal delayType    : std_logic_vector(2 downto 0) := "00";
+    signal IntSequance   : std_logic_vector(1 downto 0); -- int sequance is   : push pc,push fr,load pc
+    signal ResetSequance : std_logic_vector(1 downto 0); -- reset sequance is : load pc 
 
+    signal shouoldINT : std_logic := '0';
 begin
 
-    latch <= nop when flush = '1' and multi = '1'
+    latch_inst <= 
+         RST_INST when ResetSequance /= "00"
+    else INT_INST when IntSequance   /= "00"
+    else nop when flush = '1' and multi = '1'
+    else nop when delay /= "00"
     else half & InstIn when multi = '1'
     else nop when InstIn(15 downto 11) = MULTI_1 -- op-code == ADDI/LDM/LDD/STD
     else nop when InstIn(15 downto 11) = MULTI_2
@@ -36,11 +52,59 @@ begin
     else nop when InstIn(15 downto 11) = MULTI_4
     else InstIn & filler;
 
+    latch_intSig <= 
+         "1001" when INT_INST  = "11"
+    else "1010" when INT_INST  = "10"
+    else "1011" when INT_INST  = "01"
+    else "0000";
+
     process (clk) begin
         if (clk'event and clk = '1') then
-            InstOut <= latch;
+            if (RESET = '1') then
+                ResetSequance <= "11";
+                -- todo : fix this logic ..
+            else
+                if (ResetSequance = "11") then
+                    ResetSequance <= "10";
+                end if;
+                if (ResetSequance = "10") then
+                    ResetSequance <= "01";
+                end if;
+                if (ResetSequance = "01") then
+                    ResetSequance <= "00";
+                end if;
+                if (ResetSequance = "00") then
+                    
+                end if;
+            end if;
             half <= InstIn;
 
+            InstOut <= latch_inst;
+            intSigOut <= latch_intSig;
+
+            if (latch_inst(31 downto 27) = "11111") then -- op code of swap
+
+            end if;
+
+            
+
+            if (INT = '1') then
+                IntSequance <= "11";
+            else
+                if (IntSequance = "11") then
+                    IntSequance <= "10";
+                end if;
+                if (IntSequance = "10") then
+                    IntSequance <= "01";
+                end if;
+                if (IntSequance = "01") then
+                    IntSequance <= "00";
+                end if;
+                if (IntSequance = "00") then
+                    IntSequance <= "00";
+                end if;
+            end if;
+            
             if (multi = '1') then
                 multi <= '0';
             else
